905bd20df6e83f443dd820e16a34c3b2bf321e21
sachin budni <sachinswapna143@outlook.com>
2018-06-18 14:52:13 +0530

    time and date is adding in employee

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from './employee.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee',
@@ -7,14 +9,31 @@ import { EmployeeService } from './employee.service';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
-
-  constructor(private employeeService:EmployeeService) { }
+  user : Observable<any>;
+  email : string;
+  lastSignin : string;
+  createTime:string;
+  date = new Date();
+  constructor(private employeeService:EmployeeService,private route:Router) { }
 
   ngOnInit() {
+    this.user = this.employeeService.getUser();
+    this.user.subscribe(ref=>{
+      if(!ref){
+        this.route.navigate(['/login']);
+      }else{
+        console.log(ref);
+        this.email = ref.email;
+        this.lastSignin = ref.metadata.lastSignInTime;
+        this.createTime = ref.metadata.creationTime;
+        this.date = new Date(this.lastSignin);
+      }
+    });
   }
-
+  
   emailSigOut(){
     this.employeeService.singOut();
   }
+  
 
 }
--- src/app/employee/employee.service.ts
+++ src/app/employee/employee.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  user:any;
+  user:Observable<any>;
   constructor(private afAuth:AngularFireAuth,private route:Router) {
-      this.afAuth.authState.subscribe(ref=>{
-        if(!ref){
-          this.route.navigate(['/login'])
-        }else{
-          console.log(ref);
-          this.user = ref;
-        }
-      });
+      this.user=this.afAuth.authState;
    }
-
+   setUser(lock){
+     this.user =lock;
+   }
+getUser(){
+  return this.user;
+}
   singOut(){
     return this.afAuth.auth.signOut();
   }
